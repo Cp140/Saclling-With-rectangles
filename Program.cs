c158bcd3e729b26a49525ef3d41df7828b143735
Cole Palmer <PalmCole551@GoTVDSB.ca>
2022-05-27 10:02:42 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Saclling_With_rectangles
+{
+    public static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            using (var game = new Game1())
+                game.Run();
+        }
+    }
+}
